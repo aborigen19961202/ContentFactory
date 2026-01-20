64382ebf139e8750e4c0e1316bbb92634636fb6e
Andriy <aborigen19961202@gmail.com>
2026-01-20 16:25:34 +0200

    Fix Ukrainian text encoding

--- wiki/docusaurus.config.js
+++ wiki/docusaurus.config.js
@@ -1,4 +1,4 @@
-﻿// @ts-check
+// @ts-check
 // `@type` JSDoc annotations allow editor autocompletion and type checking
 // (when paired with `@ts-check`).
 // There are various equivalent ways to declare your Docusaurus config.
